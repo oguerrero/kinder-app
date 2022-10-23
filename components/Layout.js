aa31173836c22f692e4b12f3b64bddba513212db
oguerrero <oscarguerrero242@gmail.com>
2022-10-23 15:45:42 -0500

    add home info

--- components/Layout.js
+++ components/Layout.js
@@ -16,6 +16,12 @@ export function Layout({ children }) {
           alt='Instalaciones idoneas para el desarrollo de los pequeños'
           layout='responsive'
         />
+        <h1 class='hidden md:flex absolute text-5xl truncate bg-black bg-opacity-30 py-6 px-8 rounded-full text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+          Ternura y Educación desde 1989
+        </h1>
+        <h1 class='md:hidden absolute text-xl truncate bg-black bg-opacity-30 py-6 px-8 rounded-full text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-44'>
+          Ternura y Educación desde 1989
+        </h1>
       </div>
       <main className='max-w-sm lg:max-w-3xl m-auto'>{children}</main>
       <Galeria />
